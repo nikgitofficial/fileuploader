a593d57b726cab62986742d3d3060f6ee43f8c75
nikgitofficial <npaceno@611global.com>
2025-06-20 02:03:42 +0800

    Add /logout route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -9,4 +9,14 @@ router.post('/register', register);
 // POST /api/auth/login
 router.post('/login', login);
 
+
+router.post('/logout', (req, res) => {
+  res.clearCookie('refreshToken', {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'None'
+  });
+  return res.status(200).json({ message: 'Logged out successfully' });
+});
+
 export default router;
