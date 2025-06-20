babeb6f9a370b3fc4d82d6b0a2ff1e83ab2cebf9
nikgitofficial <npaceno@611global.com>
2025-06-21 03:07:19 +0800

    date update

--- backend/models/File.js
+++ backend/models/File.js
@@ -4,9 +4,10 @@ const fileSchema = new mongoose.Schema({
   filename: String,
   url: String,
   public_id: String,
-  type: String, 
-  uploadedAt: { type: Date, default: Date.now },
+  type: String,
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+}, {
+  timestamps: true // <-- this adds createdAt and updatedAt
 });
 
 export default mongoose.model('File', fileSchema);
