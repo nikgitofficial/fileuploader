5cc35c45abb3a754615948de914c213d5d90f296
nikgitofficial <npaceno@611global.com>
2025-06-26 00:10:31 +0800

    commit 1210

--- backend/controllers/otpController.js
+++ backend/controllers/otpController.js
@@ -7,10 +7,12 @@ export const sendOtp = async (req, res) => {
   const { email } = req.body;
   if (!email) return res.status(400).json({ error: 'Email is required' });
 
-  const existingOtp = await OtpToken.findOne({ email });
-  if (existingOtp && existingOtp.expiresAt > new Date()) {
-    return res.status(429).json({ error: 'Please wait before requesting another OTP.' });
-  }
+//
+//  const existingOtp = await OtpToken.findOne({ email });
+//if (existingOtp && existingOtp.expiresAt > new Date()) {
+//return res.status(429).json({ error: 'Please wait before requesting another OTP.' });
+//}
+//
 
   // ✅ Generate OTP and expiry
   const otp = Math.floor(100000 + Math.random() * 900000).toString();
